3cfa24927b2a8105ff235561246efc50d56d9b87
Nina Pantelic <nina.pantelic20@gmail.com>
2022-11-15 23:48:18 +0100

    Dodavanje gosta

--- handler/add.php
+++ handler/add.php
@@ -6,16 +6,17 @@ require "../model/gost.php";
 if(isset($_POST['name']) && isset($_POST['surname']) 
 && isset($_POST['email']) && isset($_POST['phone'])){
 
-    
+   
     $gost = new Gost(null,$_POST['name'],$_POST['surname'],$_POST['email'],$_POST['phone'] );
     //moze null za id jer radi svakako autoincrement
+    
     $status = Gost::add($gost, $conn);
 
     if($status){
-        echo 'Uspesno dodat gost!';
+        echo 'Success';
     }else{
         echo $status;
-        echo "Nesupesno dodat gost!";
+        echo "Failed";
     }
 }
 
